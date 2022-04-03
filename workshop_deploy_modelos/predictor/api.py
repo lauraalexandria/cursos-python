e118b5baa270c1a6fa6b1dbd37547ce7e68b728a
Laura Alexandria de Oliveira <57160675+lauraalexandria@users.noreply.github.com>
2022-04-02 21:00:36 -0300

    Add files via upload

--- workshop_deploy_modelos/predictor/api.py
+++ workshop_deploy_modelos/predictor/api.py
@@ -1,8 +1,15 @@
+import sys
+import os
+
+sys.path.insert(1, os.getcwd())
+
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel
 
 import uvicorn
+
+from predict import Predict
 
 app = FastAPI()
 
@@ -12,9 +19,13 @@ class Passenger(BaseModel): # Entrada dos valores que serão utilizados para pre
     Sex: str
     Age: int
 
-@app.get('/predict')
+@app.post('/predict')      # Tem que ser um método post, já que há parâmetros para serem passados
 def (passenger: Passenger):
     data = pd.DataFrame([vars(passenger)])
+    id = passenger.PassengerId
+    del data['PassengerId']
+    status, result = predictor.predict(data)
+    return JSONResponse({"id": int(id), "score": f'{result*100:.2f}%', "predicao": status})
 
 
 if __name__ == "__main__":
